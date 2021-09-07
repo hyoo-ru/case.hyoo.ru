5718d56b57a63ae3ccc13a6890b3d2cf108b60fd
nin-jin <nin-jin@users.noreply.github.com>
2021-09-07 09:00:37 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.esm.js
+++ web.esm.js
@@ -9759,14 +9759,12 @@ var $;
     (function ($$) {
         const { rem } = $.$mol_style_unit;
         $.$mol_style_define($$.$hyoo_case_entity_page, {
+            maxWidth: rem(60),
             Config: {
                 padding: 0,
             },
             Snippet: {
                 textShadow: '0 0',
-            },
-            flex: {
-                basis: rem(30),
             },
             Body: {
                 padding: 0,
