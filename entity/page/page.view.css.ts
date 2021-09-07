68901c30a00b65ea0606539e9e2861ed9256952f
jin <jin@hyoo.ru>
2021-09-07 11:58:46 +0300

    Dynamic page size

--- entity/page/page.view.css.ts
+++ entity/page/page.view.css.ts
@@ -4,16 +4,14 @@ namespace $.$$ {
 	
 	$mol_style_define( $hyoo_case_entity_page, {
 
+		maxWidth: rem(60),
+
 		Config: {
 			padding: 0,
 		},
 
 		Snippet: {
 			textShadow: '0 0',
-		},
-		
-		flex: {
-			basis: rem(30),
 		},
 		
 		Body: {
