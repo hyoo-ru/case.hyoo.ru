f3eadccf31d35ad6ccd53960b292983fc27ae6ec
nin-jin <nin-jin@users.noreply.github.com>
2021-12-03 07:07:13 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.d.ts
+++ node.d.ts
@@ -3580,7 +3580,7 @@ declare namespace $.$$ {
         icon(): string;
         host(): string;
         title(): string;
-        sub(): any[];
+        sub(): readonly any[];
     }
 }
 
--- web.d.ts
+++ web.d.ts
@@ -3583,7 +3583,7 @@ declare namespace $.$$ {
         icon(): string;
         host(): string;
         title(): string;
-        sub(): any[];
+        sub(): readonly any[];
     }
 }
 
