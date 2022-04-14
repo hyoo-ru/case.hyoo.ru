13e319fce7a80293dccd6b0eb15bef3d96767f79
nin-jin <nin-jin@users.noreply.github.com>
2022-04-14 07:11:43 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.test.js
+++ node.test.js
@@ -9774,6 +9774,9 @@ var $;
                 return val;
             return "";
         }
+        option_label_default() {
+            return "";
+        }
         Option_row(id) {
             const obj = new this.$.$mol_button_minor();
             obj.event_click = (event) => this.event_select(id, event);
@@ -9973,7 +9976,7 @@ var $;
             }
             option_label(id) {
                 const value = this.dictionary()[id];
-                return value == null ? id : value;
+                return (value == null ? id : value) || this.option_label_default();
             }
             option_rows() {
                 return this.options_filtered().map((option) => this.Option_row(option));
@@ -10007,7 +10010,7 @@ var $;
             trigger_content() {
                 return [
                     ...this.option_content(this.value()),
-                    ...!this.value() ? [this.Trigger_icon()] : [],
+                    this.Trigger_icon(),
                 ];
             }
             menu_content() {
