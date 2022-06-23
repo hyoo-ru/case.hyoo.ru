fdbbf992fba2d175458a5268c77463c5ff8ee32f
nin-jin <nin-jin@users.noreply.github.com>
2022-06-23 07:10:46 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.esm.js
+++ node.esm.js
@@ -8883,6 +8883,7 @@ var $;
             const obj = new this.$.$mol_text();
             obj.uri_resolve = (id) => this.uri_resolve(id);
             obj.text = () => this.quote_text(id);
+            obj.highlight = () => this.highlight();
             obj.auto_scroll = () => null;
             return obj;
         }
@@ -8890,6 +8891,7 @@ var $;
             const obj = new this.$.$mol_text();
             obj.uri_resolve = (id) => this.uri_resolve(id);
             obj.text = () => this.list_text(id);
+            obj.highlight = () => this.highlight();
             obj.auto_scroll = () => null;
             return obj;
         }
@@ -8921,6 +8923,7 @@ var $;
         Table_cell(id) {
             const obj = new this.$.$mol_text();
             obj.auto_scroll = () => null;
+            obj.highlight = () => this.highlight();
             obj.uri_resolve = (id) => this.uri_resolve(id);
             obj.text = () => this.table_cell_text(id);
             return obj;
@@ -8978,6 +8981,9 @@ var $;
         quote_text(id) {
             return "";
         }
+        highlight() {
+            return "";
+        }
         list_text(id) {
             return "";
         }
@@ -8988,9 +8994,6 @@ var $;
             return {};
         }
         code_text(id) {
-            return "";
-        }
-        highlight() {
             return "";
         }
         code_sidebar_showed() {
--- node.test.js
+++ node.test.js
@@ -8875,6 +8875,7 @@ var $;
             const obj = new this.$.$mol_text();
             obj.uri_resolve = (id) => this.uri_resolve(id);
             obj.text = () => this.quote_text(id);
+            obj.highlight = () => this.highlight();
             obj.auto_scroll = () => null;
             return obj;
         }
@@ -8882,6 +8883,7 @@ var $;
             const obj = new this.$.$mol_text();
             obj.uri_resolve = (id) => this.uri_resolve(id);
             obj.text = () => this.list_text(id);
+            obj.highlight = () => this.highlight();
             obj.auto_scroll = () => null;
             return obj;
         }
@@ -8913,6 +8915,7 @@ var $;
         Table_cell(id) {
             const obj = new this.$.$mol_text();
             obj.auto_scroll = () => null;
+            obj.highlight = () => this.highlight();
             obj.uri_resolve = (id) => this.uri_resolve(id);
             obj.text = () => this.table_cell_text(id);
             return obj;
@@ -8970,6 +8973,9 @@ var $;
         quote_text(id) {
             return "";
         }
+        highlight() {
+            return "";
+        }
         list_text(id) {
             return "";
         }
@@ -8980,9 +8986,6 @@ var $;
             return {};
         }
         code_text(id) {
-            return "";
-        }
-        highlight() {
             return "";
         }
         code_sidebar_showed() {
