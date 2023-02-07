8e3650c2d471323211697292eabc2374beb3359c
nin-jin <nin-jin@users.noreply.github.com>
2023-02-07 07:09:59 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.d.ts
+++ node.d.ts
@@ -1586,6 +1586,7 @@ declare namespace $ {
         line_height(): number;
         letter_width(): number;
         width_limit(): number;
+        row_width(): number;
         sub(): readonly any[];
     }
 }
--- web.d.ts
+++ web.d.ts
@@ -1587,6 +1587,7 @@ declare namespace $ {
         line_height(): number;
         letter_width(): number;
         width_limit(): number;
+        row_width(): number;
         sub(): readonly any[];
     }
 }
