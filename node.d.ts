f6e1152fc713f64290328f9195b5335de1d3af9c
nin-jin <nin-jin@users.noreply.github.com>
2023-03-01 07:11:22 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.d.ts
+++ node.d.ts
@@ -1810,9 +1810,9 @@ declare namespace $ {
         static href_normal(): string;
         static dict(next?: {
             [key: string]: string | null;
-        }): {
+        }): Readonly<{
             [key: string]: string;
-        };
+        }>;
         static value(key: string, next?: string | null): string | null;
         static link(next: any): string;
         static make_link(next: {
@@ -2610,6 +2610,7 @@ declare namespace $ {
         length_max(): number;
         type(val?: any): string;
         event_change(event?: any): any;
+        submit_with_ctrl(): boolean;
         submit(event?: any): any;
         Submit(): $$.$mol_hotkey;
     }
@@ -2692,6 +2693,7 @@ declare namespace $ {
         spellcheck(): boolean;
         length_max(): number;
         selection(val?: any): readonly number[];
+        submit(next?: any): any;
         bring(): void;
         Edit(): $mol_textarea_edit;
         row_numb(id: any): number;
@@ -2759,20 +2761,22 @@ declare namespace $ {
     class $mol_number extends $mol_view {
         precision_view(): number;
         precision_change(): number;
-        value(val?: any): number;
+        value_min(): number;
+        value_max(): number;
+        value(next?: any): number;
+        enabled(): boolean;
         sub(): readonly any[];
         precision(): number;
         type(): string;
         value_string(val?: any): string;
         hint(): string;
-        enabled(): boolean;
         string_enabled(): boolean;
         String(): $$.$mol_string;
-        event_dec(val?: any): any;
+        event_dec(next?: any): any;
         dec_enabled(): boolean;
         dec_icon(): $mol_icon_minus;
         Dec(): $mol_button_minor;
-        event_inc(val?: any): any;
+        event_inc(next?: any): any;
         inc_enabled(): boolean;
         inc_icon(): $mol_icon_plus;
         Inc(): $mol_button_minor;
@@ -2784,9 +2788,12 @@ declare namespace $ {
 
 declare namespace $.$$ {
     class $mol_number extends $.$mol_number {
+        value_limited(next?: any): number;
         event_dec(next?: Event): void;
         event_inc(next?: Event): void;
         value_string(next?: string): string;
+        dec_enabled(): boolean;
+        inc_enabled(): boolean;
     }
 }
 
--- node.test.js
+++ node.test.js
@@ -8858,6 +8858,9 @@ var $;
                 return event;
             return null;
         }
+        submit_with_ctrl() {
+            return false;
+        }
         submit(event) {
             if (event !== undefined)
                 return event;
@@ -8865,6 +8868,7 @@ var $;
         }
         Submit() {
             const obj = new this.$.$mol_hotkey();
+            obj.mod_ctrl = () => this.submit_with_ctrl();
             obj.key = () => ({
                 enter: (event) => this.submit(event)
             });
@@ -9070,6 +9074,11 @@ var $;
                 return val;
             return [];
         }
+        submit(next) {
+            if (next !== undefined)
+                return next;
+            return null;
+        }
         bring() {
             return this.Edit().bring();
         }
@@ -9081,6 +9090,8 @@ var $;
             obj.spellcheck = () => this.spellcheck();
             obj.length_max = () => this.length_max();
             obj.selection = (val) => this.selection(val);
+            obj.submit = (next) => this.submit(next);
+            obj.submit_with_ctrl = () => true;
             return obj;
         }
         row_numb(id) {
@@ -9114,6 +9125,9 @@ var $;
     __decorate([
         $mol_mem
     ], $mol_textarea.prototype, "selection", null);
+    __decorate([
+        $mol_mem
+    ], $mol_textarea.prototype, "submit", null);
     __decorate([
         $mol_mem
     ], $mol_textarea.prototype, "Edit", null);
@@ -9292,10 +9306,19 @@ var $;
         precision_change() {
             return this.precision();
         }
-        value(val) {
-            if (val !== undefined)
-                return val;
+        value_min() {
+            return -Infinity;
+        }
+        value_max() {
+            return +Infinity;
+        }
+        value(next) {
+            if (next !== undefined)
+                return next;
             return +NaN;
+        }
+        enabled() {
+            return true;
         }
         sub() {
             return [
@@ -9318,9 +9341,6 @@ var $;
         hint() {
             return " ";
         }
-        enabled() {
-            return true;
-        }
         string_enabled() {
             return this.enabled();
         }
@@ -9332,9 +9352,9 @@ var $;
             obj.enabled = () => this.string_enabled();
             return obj;
         }
-        event_dec(val) {
-            if (val !== undefined)
-                return val;
+        event_dec(next) {
+            if (next !== undefined)
+                return next;
             return null;
         }
         dec_enabled() {
@@ -9346,16 +9366,16 @@ var $;
         }
         Dec() {
             const obj = new this.$.$mol_button_minor();
-            obj.event_click = (val) => this.event_dec(val);
+            obj.event_click = (next) => this.event_dec(next);
             obj.enabled = () => this.dec_enabled();
             obj.sub = () => [
                 this.dec_icon()
             ];
             return obj;
         }
-        event_inc(val) {
-            if (val !== undefined)
-                return val;
+        event_inc(next) {
+            if (next !== undefined)
+                return next;
             return null;
         }
         inc_enabled() {
@@ -9367,7 +9387,7 @@ var $;
         }
         Inc() {
             const obj = new this.$.$mol_button_minor();
-            obj.event_click = (val) => this.event_inc(val);
+            obj.event_click = (next) => this.event_inc(next);
             obj.enabled = () => this.inc_enabled();
             obj.sub = () => [
                 this.inc_icon()
@@ -9419,31 +9439,54 @@ var $;
     var $$;
     (function ($$) {
         class $mol_number extends $.$mol_number {
+            value_limited(next) {
+                if (next === undefined)
+                    return this.value();
+                if (next === '')
+                    return this.value(null);
+                const min = this.value_min();
+                const max = this.value_max();
+                const val = Number(next);
+                if (val < min)
+                    return this.value(min);
+                if (val > max)
+                    return this.value(max);
+                return this.value(val);
+            }
             event_dec(next) {
-                this.value((Number(this.value()) || 0) - this.precision_change());
+                this.value_limited((this.value_limited() || 0) - this.precision_change());
             }
             event_inc(next) {
-                this.value((Number(this.value()) || 0) + this.precision_change());
+                this.value_limited((this.value_limited() || 0) + this.precision_change());
             }
             value_string(next) {
-                if (next !== void 0) {
-                    this.value(next === '' ? null : Number(next));
-                }
+                const next_num = this.value_limited(next);
                 const precisionView = this.precision_view();
-                const value = next ? Number(next) : this.value();
-                if (value === 0)
+                if (next_num === 0)
                     return '0';
-                if (!value)
+                if (!next_num)
                     return '';
                 if (precisionView >= 1) {
-                    return (value / precisionView).toFixed();
+                    return (next_num / precisionView).toFixed();
                 }
                 else {
                     const fixedNumber = Math.log10(1 / precisionView);
-                    return value.toFixed(fixedNumber);
+                    return next_num.toFixed(Math.ceil(fixedNumber));
                 }
             }
+            dec_enabled() {
+                return this.enabled() && (!((this.value() || 0) <= this.value_min()));
+            }
+            inc_enabled() {
+                return this.enabled() && (!((this.value() || 0) >= this.value_max()));
+            }
         }
+        __decorate([
+            $mol_mem
+        ], $mol_number.prototype, "dec_enabled", null);
+        __decorate([
+            $mol_mem
+        ], $mol_number.prototype, "inc_enabled", null);
         $$.$mol_number = $mol_number;
     })($$ = $.$$ || ($.$$ = {}));
 })($ || ($ = {}));
